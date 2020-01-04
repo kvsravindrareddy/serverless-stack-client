61cff4c330cbb15a3c043564e47c3ae3a67f5b58
kvsravindrareddy <kvsravindrareddy@gmail.com>
2020-01-04 06:33:47 +0530

    react changes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "kvsrrnotes"
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://f3m1b066ij.execute-api.ap-south-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_9oMTA9XDB",
+    APP_CLIENT_ID: "55dag15pb9hmrhkvc5o4p4cg8l",
+    IDENTITY_POOL_ID: "ap-south-1:086bf1cd-bc23-4e61-ab18-06f49cbda1a0"
+  }
+};
